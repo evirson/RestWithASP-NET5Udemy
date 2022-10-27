71db5d924f06e1a0f8f3bf8baf12754cccdac65e
evirson <evirson@vetorsistemas.com.br>
2022-10-27 19:34:27 -0300

    implementando generics

---
+++ SlnEstudoRest/EstudoRest/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using EstudoRest.Model;
+using EstudoRest.Model.Base;
+using EstudoRest.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoRest.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySQLContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext context) 
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public void Delete(long id)
+        {
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SlnEstudoRest/EstudoRest/Repository/Generic/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using EstudoRest.Model;
+using EstudoRest.Model.Base;
+
+namespace EstudoRest.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        Person FindById(long id);
+
+        List<T> FindAll();
+
+        Person Update(T item);
+
+        void Delete(long id);
+
+        bool Exists(long id);
+
+    }
+}
