3a93a8976e0112a7bceb1a9c6f27d21f46408c65
evirson <evirson@vetorsistemas.com.br>
2022-12-13 18:13:58 -0300

    13-12-2022 iniciando desenho da tela de login

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+import Login from './pages/Login';
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> e salva para reload de novo
-        </p>
-        <a className='App-link' href="http://vetorsistemas.com.br" target="_blank">Teste</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login/>
+   
   );
+
 }
 
-export default App;
+
+
+
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.css'
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <section className='form'>
+            <img src={logoImage} alt="Erudio Logo"/>
+            <form>
+                <h1>Access Yout Account</h1>
+
+                <input placeholder="UserName"/>
+                <input type="password" placeholder="Password"/>
+
+                <button type="submit">Login</button> 
+            </form>
+
+            </section>
+            <img src={padlock} alt="Login"/>
+        </div>
+
+    )
+
+}
